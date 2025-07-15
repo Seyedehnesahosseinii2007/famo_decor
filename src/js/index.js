4c85f6e5b04b65388a01cd5c43daa7c87d7c2770
Seyedeh Nesa Hosseini <seyedehnesahosseiniofficiall@outlook.com>
2025-07-15 15:06:51 +0330

    اه

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.addEventListener('DOMContentLoaded', function () {
     // آرایه تایتل‌ها و بخش‌های محصولات به ترتیب
     const headings = [
@@ -55,3 +56,22 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+=======
+"use strict";
+const vaseelement = document.getElementsByClassName("menu1");
+const menuBTN = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+// for humburger menu close and open 
+if (menuBTN && menu) {
+    menuBTN.addEventListener('click', () => {
+        menuBTN.classList.toggle('open');
+        menu.classList.toggle('open');
+    });
+}
+document.onclick = (event) => {
+    if (menu && menuBTN && !menu.contains(event.target) && !menuBTN.contains(event.target)) {
+        menuBTN.classList.remove('open');
+        menu.classList.remove('open');
+    }
+};
+>>>>>>> parent of fcc8c6c (product detail)
