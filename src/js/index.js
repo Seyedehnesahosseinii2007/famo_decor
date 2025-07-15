1524d5e58c80ff59500053ae37be67d9457a1e15
Seyedeh Nesa Hosseini <seyedehnesahosseiniofficiall@outlook.com>
2025-07-15 13:49:45 +0330

    hello

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,21 @@
+"use strict";
+const vaseelement = document.getElementsByClassName("menu1");
+const menuBTN = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+// for humburger menu close and open 
+if (menuBTN && menu) {
+    menuBTN.addEventListener('click', () => {
+        menuBTN.classList.toggle('open');
+        menu.classList.toggle('open');
+    });
+}
+document.onclick = (event) => {
+    if (menu && menuBTN && !menu.contains(event.target) && !menuBTN.contains(event.target)) {
+        menuBTN.classList.remove('open');
+        menu.classList.remove('open');
+    }
+};
+
 document.addEventListener('DOMContentLoaded', function () {
     // آرایه عنوان‌ها و بخش‌ها به ترتیب
     var headings = [
